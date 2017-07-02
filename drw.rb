63802dc2c190c0496dc85b8780e06b57bc556a5b
m3y <ma3ya.ozw+github@gmail.com>
2017-07-03 03:45:43 +0900

    modify archive tags

--- drw.rb
+++ drw.rb
@@ -5,8 +5,8 @@
 class Drw < Formula
   desc "docker run wrapper"
   homepage "https://github.com/m3y/docker-run-wrapper"
-  url "https://github.com/m3y/docker-run-wrapper/archive/v1.0.tar.gz"
-  sha256 "194483ef03a2b7762519fd84331318af293822fd6f2459af6abde61bf2c7a113"
+  url "https://github.com/m3y/docker-run-wrapper/archive/v0.1tar.gz"
+  sha256 "275c348704dc272cc160be324fded94cea0ed930b10cca91b687d61494597255"
 
   def install
     bin.install "drw"
