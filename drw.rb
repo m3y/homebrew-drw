24b1b0f23edc18d38194b29258beac48927c3989
m3y <ma3ya.ozw+github@gmail.com>
2017-12-03 15:47:58 +0900

    インストール時にコマンド名を変更する対応

--- drw.rb
+++ drw.rb
@@ -1,14 +1,10 @@
-# Documentation: http://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Drw < Formula
   desc "docker run wrapper"
   homepage "https://github.com/m3y/docker-run-wrapper"
-  url "https://github.com/m3y/docker-run-wrapper/archive/v0.1.tar.gz"
-  sha256 "b6bab9cc73b3dbf493767f1af68c04c725c6a70ca77c271ae9f1d20852d2af32"
+  url "https://github.com/m3y/docker-run-wrapper/archive/v0.2.tar.gz"
+  sha256 "cdf8130ec0914d32fc20fc1465da3ce0b3fd5382e3f45bfda21055468cce87f5"
 
   def install
-    bin.install "drw"
+    bin.install "docker_run_wrapper.py" => "drw"
   end
 end
